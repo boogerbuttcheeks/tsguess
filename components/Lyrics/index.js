aa338828da96c04ceff7ee1107723124b9e6f098
Trevor Lee <tlee297@my.bcit.ca>
2023-02-07 23:16:57 -0800

    add animation, first line not animating

--- components/Lyrics/index.js
+++ components/Lyrics/index.js
@@ -1,9 +1,26 @@
+import styles from './Lyrics.module.css'
+import { useEffect, useRef } from "react"
+
 export default function Lyrics({ lyrics, guessesRemaining }) {
+  const firstLineRef = useRef(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (firstLineRef.current) {
+        firstLineRef.current.classList.add(styles.fadeIn);
+      }
+    }, 0);
+  }, [])
+
   return (
-    <>
-      {lyrics && <p>{lyrics.prev}</p>}
-      {guessesRemaining <= 2 && lyrics && <p>{lyrics.lyric}</p>}
-      {guessesRemaining <= 1 && lyrics && <p>{lyrics.next}</p>}
-    </>
+    <div>
+      {lyrics && <p ref={firstLineRef}>{lyrics.prev}</p>}
+      {guessesRemaining <= 2 && lyrics && <p className={
+        guessesRemaining <= 2 ? `${styles.fadeIn}` : ''
+      }>{lyrics.lyric}</p>}
+      {guessesRemaining <= 1 && lyrics && <p className={
+        guessesRemaining <= 1 ? `${styles.fadeIn}` : ''
+      }>{lyrics.next}</p>}
+    </div>
   )
 }
